7caa6be05eb2bf6793197b531f50a81e2645f0a2
Zhuo Liu <zliu7163@gmail.com>
2024-09-28 17:14:56 -0700

    Added specification of AllItems class

--- src/main/model/AllItems.java
+++ src/main/model/AllItems.java
@@ -1,5 +1,48 @@
 package model;
 
+import java.util.ArrayList;
+
+// Represents the List of ALL items added but not deleted by stuff,
+// regardless of its availibility.
+
 public class AllItems {
 
+    // EFFECTS: initialize an arrayList of items
+    public AllItems() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add an item to AllItems
+    public void addItem(Item item) {
+        //stub
+    }
+
+    // REQUIRES: the given item exists
+    // MODIFIES: this
+    // EFFECTS: remove an item in AllItems 
+    public void removeItem(Item item) {
+        //stub
+    }
+
+    // REQUIRES: the given item exists
+    // MODIFIES: this Item
+    // EFFECTS: mutate the name and price of an item
+    public void mutateItem(Item item) {
+        //stub
+    }
+
+    // EFFECTS: produce the item corresponding to the itemName, otherwise return null 
+    public Item findItem(String itemName) {
+        return null;
+    }
+
+    // EFFECTS: produce the list of all items in String, in a consistent format
+    public String printItems() {
+        return "";
+    }
+
+    public ArrayList<Item> getAllItems() {
+        return null;
+    }
 }
