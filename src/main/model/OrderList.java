828f07ea64ceed9c1121fd974b15b1a4ccdeb99a
Zhuo Liu <zliu7163@gmail.com>
2024-09-28 21:07:18 -0700

    Added specification of OrderList class

--- src/main/model/OrderList.java
+++ src/main/model/OrderList.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
+// Represents the ArrayList of finished orders
+
 public class OrderList {
 
+    //EFFECTS: initialize an empty ArrayList of Order
+    public OrderList() {
+        //stub
+    }
+
+    //EFFECTS: print the receipt of all order in String in OrderList
+    public String displayOrderList() {
+        return "";
+    }
+
+    public ArrayList<Order> getOrderList() {
+        return null;
+    }
 }
