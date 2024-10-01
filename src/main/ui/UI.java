3dc10388bb4c3006b3fb42e301b45046dd8db5ad
Zhuo Liu <zliu7163@gmail.com>
2024-10-01 10:49:29 -0700

    Added the specification of the UI class

---
+++ src/main/ui/UI.java
@@ -0,0 +1,32 @@
+package ui;
+
+import model.Order;
+
+public class UI {
+
+    //EFFECTS: display the menu that allows user to select type of user 
+    // (stuff or customer)
+    public UI() {
+
+    }
+
+    //EFFECTS: display the menu of UI for stuff and allow stuff to realize user stories
+    public void stuffUI() {
+
+    }
+
+    //EFFECTS: display the menu of UI for customers and allow customers to realize user stories
+    public void customerUI() {
+
+    }
+
+    //EFFECTS: produce true if the item with the given name exists in AllItems, otherwise produce false
+    public boolean checkExistanceAllItems(String name) {
+        return false;
+    }
+
+    //EFFECTS: produce true if the item with the given name exists in a given order, otherwise produce false
+    public boolean checkExistanceOrder(String name,Order odr) {
+        return false;
+    }
+}
