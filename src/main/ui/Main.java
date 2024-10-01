1f514eef9d0377fb8e48015c3ee10c79406dd770
Zhuo Liu <zliu7163@gmail.com>
2024-10-01 12:25:37 -0700

    Added the specification and implementation of main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,11 @@
 package ui;
 
 public class Main {
+
+    // EFFECTS: start the application with the menu that allows user to select type of user 
+    // (stuff or customer)
     public static void main(String[] args) throws Exception {
         //System.out.println("Welcome to my project!");
+        new UI();
     }
 }
