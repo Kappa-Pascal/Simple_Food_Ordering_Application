b906591868c7782e6104e4e3e778aa5e8ba9a383
Zhuo Liu <zliu7163@gmail.com>
2024-10-01 12:27:15 -0700

    Implemented the stuffUI method and its helpers

--- src/main/ui/UI.java
+++ src/main/ui/UI.java
@@ -1,32 +1,157 @@
 package ui;
 
+import model.AllItems;
+import model.Item;
 import model.Order;
+import model.OrderList;
+
+import java.util.Scanner;
 
 public class UI {
+    private AllItems allItems;
+    private OrderList orderList;
+    private Scanner in;
+    private static final String textUI = "Welcome to the Food Ordering Application\n"
+            + "s: Go to the stuff memu\n"
+            + "c: Go to the customer menu";
+    private static final String errorMessage = "Invalid input, please try again";
+    private static final String itemNotFoundMessage = "The give Item is not found";
+    private static final String textStuffUI = "Welcome to the stuff menu\n"
+            + "ai: Add a new item to the Item list\n"
+            + "as: Add the stock to the given item\n"
+            + "rm: Remove given item\n"
+            + "mt: mutate given item\n"
+            + "vi: view item list\n"
+            + "vo: view order list\n"
+            + "ex: go back to the previous menu";
 
-    //EFFECTS: display the menu that allows user to select type of user 
+    // EFFECTS: display the menu that allows user to select type of user
     // (stuff or customer)
     public UI() {
-
+        in = new Scanner(System.in);
+        allItems = new AllItems();
+        orderList = new OrderList();
+        while (true) {
+            System.out.println(textUI);
+            String input = in.nextLine();
+            switch (input) {
+                case "s":
+                    stuffUI();
+                    break;
+                case "c":
+                    customerUI();
+                    break;
+                default:
+                    System.out.println(errorMessage);
+                    break;
+            }
+        }
     }
 
-    //EFFECTS: display the menu of UI for stuff and allow stuff to realize user stories
+    // EFFECTS: display the menu of UI for stuff and allow stuff to realize user
+    // stories of stuffs
     public void stuffUI() {
-
+        while (true) {
+            System.out.println(textStuffUI);
+            String input = in.nextLine();
+            switch (input) {
+                case "ai":
+                    addItemStuffUI();
+                    break;
+                case "as":
+                    addStockStuffUI();
+                    break;
+                case "rm":
+                    removeItemStuffUI();
+                    break;
+                case "mt":
+                    mutateItemStuffUI();
+                    break;
+                case "vi":
+                    System.out.println(allItems.printItems());
+                    break;
+                case "vo":
+                    System.out.println(orderList.displayOrderList());
+                    break;
+                case "ex":
+                    return;
+                default:
+                    System.out.println(errorMessage);
+                    break;
+            }
+        }
     }
 
-    //EFFECTS: display the menu of UI for customers and allow customers to realize user stories
+    // EFFECTS: display the menu of UI for customers and allow customers to realize
+    // user stories
     public void customerUI() {
-
+        // System.out.println("customer");
     }
 
-    //EFFECTS: produce true if the item with the given name exists in AllItems, otherwise produce false
+    // EFFECTS: produce true if the item with the given name exists in AllItems,
+    // otherwise produce false
     public boolean checkExistanceAllItems(String name) {
+        if (allItems.findItem(name) == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // EFFECTS: produce true if the item with the given name exists in a given
+    // order, otherwise produce false
+    public boolean checkExistanceOrder(String name, Order odr) {
         return false;
     }
 
-    //EFFECTS: produce true if the item with the given name exists in a given order, otherwise produce false
-    public boolean checkExistanceOrder(String name,Order odr) {
-        return false;
+    // EFFECTS: display the ui of adding items in stuff menu
+    public void addItemStuffUI() {
+        System.out.println("Input the name of item");
+        String name = in.nextLine();
+        System.out.println("Input the price of item");
+        double price = in.nextDouble();
+        System.out.println("Input the amount of stock of item");
+        int stock = in.nextInt();
+        Item item = new Item(name, price, stock);
+        allItems.addItem(item);
+    }
+
+    // EFFECTS: display the ui of adding stock in stuff menu
+    public void addStockStuffUI() {
+        System.out.println("Input the name of item you want to add stock to");
+        String name = in.nextLine();
+        if (!checkExistanceAllItems(name)) {
+            System.out.println(itemNotFoundMessage);
+        } else {
+            System.out.println("Input the amount of stock you want to add to");
+            int amount = in.nextInt();
+            allItems.findItem(name).addstock(amount);
+        }
+    }
+
+    // EFFECTS: display the ui of removing items in stuff menu
+    public void removeItemStuffUI() {
+        System.out.println("Input the name of item you want to remove");
+        String name = in.nextLine();
+        if (!checkExistanceAllItems(name)) {
+            System.out.println(itemNotFoundMessage);
+        } else {
+            allItems.removeItem(allItems.findItem(name));
+        }
+    }
+
+    // EFFECTS: display the ui of adding stock in stuff menu
+    public void mutateItemStuffUI() {
+        System.out.println("Input the name of item you want to mutate");
+        String name = in.nextLine();
+        if (!checkExistanceAllItems(name)) {
+            System.out.println(itemNotFoundMessage);
+        } else {
+            System.out.println("Input the new name of item");
+            String newName = in.nextLine();
+            System.out.println("Input the new price of item");
+            double newPrice = in.nextDouble();
+            allItems.mutateItem(allItems.findItem(name),newName,newPrice);
+        }
     }
 }
