d70c7964ca3dde119ca296db6605a0f630d382b3
Zhuo Liu <zliu7163@gmail.com>
2024-10-02 10:58:08 -0700

    Added class-level document to Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,6 @@
 package ui;
 
+// Start the application by calling to the constuctor of UI class
 public class Main {
 
     // EFFECTS: start the application with the menu that allows user to select type of user 
