d0f81445756cfd9389876c24d6be9cb4dd2a53f9
Zhuo Liu <zliu7163@gmail.com>
2024-10-13 11:52:18 -0700

    Deleted an unused import

--- src/test/model/TestJsonWriterAllItems.java
+++ src/test/model/TestJsonWriterAllItems.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 
-import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import model.Item;
